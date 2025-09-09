165b9b9a58a0778bcd8f2ef45177ca74dcf3e76b
Vladyslav Klymenko <klymenvlad@gmail.com>
2025-09-09 21:47:43 +0200

    test: added simple e2e test for reservation creating and getting

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,17 +1,66 @@
 describe('Reservations', () => {
-  const user = {
-    email: 'gamotrick@gmail.com',
-    password: 'test1234Test#',
-  };
+  let jwt: string;
 
   beforeAll(async () => {
-    await fetch('http://auth:3001', {
+    const user = {
+      email: 'gamotrick@gmail.com',
+      password: 'test1234Test#',
+    };
+
+    await fetch('http://auth:3001/users', {
       method: 'POST',
       body: JSON.stringify(user),
+      headers: { 'Content-Type': 'application/json' },
     });
 
+    const response = await fetch('http://auth:3001/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
+    jwt = await response.text();
   });
 
-  test('Create', () => {});
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', Authentication: jwt },
+      },
+    );
+
+    const reservation = await responseGet.json();
+
+    expect(reservation).toEqual(createdReservation);
+  });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', Authentication: jwt },
+        body: JSON.stringify({
+          startDate: '02-01-2023',
+          endDate: '02-03-2023',
+          charge: {
+            amount: 12343,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
 });
